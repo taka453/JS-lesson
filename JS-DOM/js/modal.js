0935fb4d06b7800730119beb499901f8685e243a
taka453 <paisley.park.39@gmail.com>
2020-07-03 17:02:31 +0900

    modal,html,css追加

---
+++ JS-DOM/js/modal.js
@@ -0,0 +1,53 @@
+// DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('js-modal');
+    const mask = document.getElementById('js-mask');
+    const button = document.getElementById('js-btn');
+    const body = document.body;
+    let clientWidth = body.clientWidth;
+    let noScrollBarWidth = clientWidth;
+    let diff = 0;
+
+    // modalを開ける
+    document.querySelector('.js-modal-open').addEventListener('click', () => {
+        // activeクラスを追加
+        modal.classList.add('active');
+        mask.classList.add('active');
+        //スクロールバーを無くす前
+        clientWidth = body.clientWidth;
+        body.style.overflow = 'hidden';
+        //スクロールバーを無くした後
+        noScrollBarWidth = body.clientWidth;
+        // スクロールバーの長さを計算
+        diff = noScrollBarWidth - clientWidth;
+        if(diff > 0) {
+            body.style['padding-right'] = diff + 'px';
+        }
+        modal.style.display = 'block';
+    });
+
+    // modalを閉じる
+    document.querySelector('#js-modal-close').addEventListener('click', () => {
+        // activeクラスを削除
+        modal.classList.remove('active');
+        mask.classList.remove('active');
+        // overflow初期化
+        body.style.overflow = 'auto';
+        body.style['padding'] = '0';
+        modal.style.display = 'none';
+    });
+
+    // modal内部画面
+    button.addEventListener('click', event => {
+        // #にならないようにする
+        event.preventDefault();
+        const textForm = document.getElementById('text-form').value;
+        if(textForm === '') {
+            window.alert('何か入力してください');
+        } else {
+            window.alert(textForm);
+        }
+    });
+}, false);
+
+
